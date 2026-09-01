a20d72ead7d8138421f358a9c2baa18e1d234ceb
seeck <shelfseeck@gmail.com>
2026-09-01 16:44:28 +0800

    refine typography and header states

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
         darkborder: '#35383f',
       },
       fontFamily: {
-        'sans': ['"Iosevka Extended"', '"Source Han Serif SC"', '"Noto Serif CJK SC"', 'serif'],
+        'sans': ['"Iosevka Extended"', '"Noto Serif SC Variable"', '"Source Han Serif SC"', '"Noto Serif CJK SC"', 'serif'],
         'mono': ['"Maple Mono"', 'monospace'],
       },
     },
